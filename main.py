8d50201f2affcc5b3d27c541105b93c0423ce718
Félix Thouin <felix.thouin@gmail.com>
2021-02-05 10:17:55 -0500

    At long last, this code works well

---
+++ main.py
@@ -0,0 +1,9 @@
+import areaCalc 
+import perimCalc 
+import numpy as np
+
+a=5
+area=areaCalc.square(a)
+perimeter=perimCalc.square(a)
+print('For a square of length %d, the area is %d and the perimeter %d'
+%(a,area,perimeter))
